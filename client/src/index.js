d9ca361a3d45a99f58a59c82fcf4e3b8d0f515ba
Elvis Chan <choectt@gmail.com>
2018-12-02 18:58:00 -0800

    Add redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import {
-  Provider
-} from 'react-redux';
-import {
-  createStore
-} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import moviesApp from './reducers/';
 import * as serviceWorker from './serviceWorker';
 
@@ -16,14 +11,10 @@ export const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-ReactDOM.render( <
-  Provider store = {
-    store
-  } >
-  <
-  App / >
-  <
-  /Provider>,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
 
