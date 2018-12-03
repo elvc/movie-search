668c41b46d45f0c608c6b261c8f3622a60990613
Elvis Chan <choectt@gmail.com>
2018-12-02 18:57:42 -0800

    Initialize app by fetching movie data, build genre options

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { setResults, setAllMovies, setOptions } from './actions';
+import Search from './Containers/Search';
+import Result from './Containers/Result';
+import Filter from './Containers/Filter';
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/api/movies')
+      .then(({ data }) => {
+        // subset of data
+        data.splice(200, 20000);
+        // initialize Store. Keep this in case we need to retrieve original data
+        this.props.setAllMovies(data);
+        // prepare for rendering results.
+        this.props.setResults(data);
+        // build options for Select element (filter)
+        this.buildGenreOptions(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  // for the filter options
+  buildGenreOptions = data => {
+    const options = [];
+    const uniqueGenre = [...new Set(data.map(movie => movie.genre))];
+    uniqueGenre.forEach(genre => {
+      if (genre) {
+        options.push({
+          value: genre,
+          label: genre,
+        });
+      }
+    });
+
+    this.props.setGenreOptions(options);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="container">
+        <header>
+          <h1 className="text-center mt-4 mb-4">Movie Catalog</h1>
+          <div className="container">
+            <div className="row">
+              <Filter />
+              <Search />
+            </div>
+          </div>
+          <Result />
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setAllMovies: payload => dispatch(setAllMovies(payload)),
+  setResults: payload => dispatch(setResults(payload)),
+  setGenreOptions: paylaad => dispatch(setOptions(paylaad)),
+});
+
+export default connect(
+  undefined,
+  mapDispatchToProps,
+)(App);
