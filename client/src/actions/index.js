b971cbda8dac3550196a71e7877d15a49402df03
Elvis Chan <choectt@gmail.com>
2018-12-02 18:53:28 -0800

    Set action index and formatting

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,27 +1,13 @@
-export {
-  SET_ALL_MOVIES,
-  setAllMovies
-}
-from './set-all-movies';
-export {
-  SET_OPTIONS,
-  setOptions
-}
-from './set-options';
+export { SET_ALL_MOVIES, setAllMovies } from './set-all-movies';
+export { SET_OPTIONS, setOptions } from './set-options';
 export {
   SET_RESULTS,
   SET_FILTER_SEARCH_RESULTS,
   setResults,
   setFilterSearchResults,
-}
-from './set-result';
-export {
-  SET_FILTER_VALUES,
-  setFilterValues
-}
-from './set-filter-values';
+} from './set-result';
+export { SET_FILTER_VALUES, setFilterValues } from './set-filter-values';
 export {
   SET_SEARCH_INPUT_VALUE,
   setSearchInputValue,
-}
-from './set-search-input-value';
+} from './set-search-input-value';
